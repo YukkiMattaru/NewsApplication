32a9d655a88fc24d6050b4cfc1a0f85de38a8ad7
YukkiMattaru <gaidukov200078@gmail.com>
2021-10-08 13:05:17 +0300

    добавлена сортировка и фильтрация

--- NewsApplication/GraphQL/Query.cs
+++ NewsApplication/GraphQL/Query.cs
@@ -9,12 +9,16 @@ namespace NewsApplication.GraphQL
     public class Query
     {
         [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Platform> GetPlatforms([ScopedService] AppDbContext context)
         {
             return context.Platforms;
         }
 
         [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Command> GetCommands([ScopedService] AppDbContext context)
         {
             return context.Commands;
